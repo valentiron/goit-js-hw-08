46566577dda4aaf35fcec1119e4592a1bafbd139
Valentyn Herasymenko <valikgera2002@gmail.com>
2022-11-11 23:39:48 +0200

    made 2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
