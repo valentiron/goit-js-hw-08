730860da8dd286cbdc97d5cdbcb5f71868487ccf
Valentyn Herasymenko <valikgera2002@gmail.com>
2022-11-19 14:23:32 +0200

    made task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,3 +7,21 @@ console.log(galleryItems);
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const gallery = document.querySelector(".gallery");
+
+  
+const img = galleryItems
+.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+        <img 
+        class="gallery__image" 
+        src="${preview}" 
+        alt="${description}"
+        />
+        </a>`
+        ).join("");
+gallery.insertAdjacentHTML("beforeend", img);
+  
+let lightbox = new SimpleLightbox('.gallery a', 
+{captionsData: 'alt', captionDelay:'250'});
+  
